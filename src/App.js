cea1df8bc4b9cc258c1e08ba6b33ab3739e5b47c
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-10-15 00:41:02 -0400

    added basic fetch setup using async to grab movie data from Movie Database website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Movie from './Movie'
+
+class App extends Component {
+
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=a1d1f9d8d405b856379b76511512345c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      })
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  render() {
+    console.log(this.state.movies)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {this.state.movies.map((movie) => {
+          return (
+          <Movie key={movie.id} movie={movie} />
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Movie extends Component {
+  // static propTypes = {
+  //   movie: PropTypes.shape({
+  //     title: PropTypes.string.isRequired,
+  //   }),
+  //   desc: PropTypes.string,
+  // }
+
+
+  render() {
+    return (
+      <div>
+        <h3>{this.props.movie.title}</h3>
+      </div>
+    )
+  }
+}
